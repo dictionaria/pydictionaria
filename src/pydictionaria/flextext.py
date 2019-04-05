8b2100d75829e711bfbd0f9d40919afc7b5156eb
Johannes Englisch <cyberjoe0815@hotmail.com>
2019-04-05 12:14:45 +0200

    flextext: added continue statements

--- src/pydictionaria/flextext.py
+++ src/pydictionaria/flextext.py
@@ -32,14 +32,16 @@ def separate_examples(document, log=None):
     for text in document.iter('interlinear-text'):
         title = get_item(text, 'title')
         vernacular, other_langs = get_languages(text)
-        paragraphs = text.find('paragraphs') or ()
+        paragraphs = text.find('paragraphs')
         if not paragraphs:
             log_or_raise("No paragraphs in interlinear text '{}'".format(text.attrib.get('guid', '???')), log)
+            continue
 
         for paragraph in paragraphs.iter('paragraph'):
-            phrases = paragraph.find('phrases') or ()
+            phrases = paragraph.find('phrases')
             if not phrases:
                 log_or_raise("No phrases in paragraph '{}'".format(paragraph.attrib.get('guid', '???')), log)
+                continue
 
             for phrase in phrases.iter('phrase'):
                 segnum = get_item(phrase, 'segnum')
