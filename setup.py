f88f25919d4c4116cdbd2fe68d0801c8b5d571c8
xrotwang <xrotwang@googlemail.com>
2019-09-23 13:58:32 +0200

    bump version for development

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pydictionaria',
-    version='1.0.0',
+    version='1.0.1.dev0',
     description='',
     long_description='',
     classifiers=[
