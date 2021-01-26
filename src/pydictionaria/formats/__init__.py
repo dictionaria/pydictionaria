a68d7705dfaa1ae2964620ab87c5b3ecb864b1a3
Johannes Englisch <englisch@shh.mpg.de>
2021-01-26 13:31:07 +0100

    dropped filemaker support

--- src/pydictionaria/formats/__init__.py
+++ src/pydictionaria/formats/__init__.py
@@ -1,5 +1,4 @@
 from pydictionaria.formats import sfm
-from pydictionaria.formats import filemaker
 from pydictionaria.formats import cldf
 
-FORMATS = [sfm.Dictionary, filemaker.Dictionary, cldf.Dictionary]
+FORMATS = [sfm.Dictionary, cldf.Dictionary]
