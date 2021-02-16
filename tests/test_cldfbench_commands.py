0db50779a1b9a7372172b5b8aa7f2a4b8f59a5c3
Johannes Englisch <englisch@shh.mpg.de>
2021-02-16 13:55:00 +0100

    tests for release command

---
+++ tests/test_cldfbench_commands.py
@@ -0,0 +1,42 @@
+from io import StringIO
+import pathlib
+import shlex
+import shutil
+
+import pytest
+
+import pydictionaria
+from cldfbench.__main__ import main
+
+
+MOCK_STDIN = """\
+testbench
+Title
+License
+URL
+Citation"""
+
+
+def _main(cmd, **kw):
+    main(['--no-config'] + shlex.split(cmd), **kw)
+
+
+@pytest.fixture
+def dataset_dir():
+    return pathlib.Path(__file__).parent / 'test_data'
+
+
+@pytest.fixture
+def tmp_dataset(dataset_dir, tmp_path, mocker):
+    mocker.patch('sys.stdin', StringIO(MOCK_STDIN))
+    _main("new --template dictionaria_submission --out '{}'".format(tmp_path))
+    bench_path = tmp_path / 'testbench'
+    shutil.copy(dataset_dir / 'intro.md', bench_path / 'raw' / 'intro.md')
+    shutil.copy(dataset_dir / 'db.sfm', bench_path / 'raw' / 'db.sfm')
+    return bench_path
+
+
+def test_release(capsys, tmp_dataset):
+    _main('dictionaria.release {}'.format(tmp_dataset / 'cldfbench_testbench.py'))
+    assert (tmp_dataset / 'README.md').exists()
+    assert (tmp_dataset / '.zenodo.json').exists()
---
+++ tests/test_data/intro.md
@@ -0,0 +1,29 @@
+# Test intro
+
+Test sentence wrapper:
+
+<div class="sentence-wrapper">
+<div class="sentence-number">
+(1)
+</div>
+<div class="sentence">
+<div class="body">
+<div class="object-language"><em>Bemme mit Brot</em></div>
+<div class="gloss-box">
+<div class="gloss-unit">
+<div class="morpheme">Bemme</div>
+<div class="gloss">sandwich</div>
+</div>
+<div class="gloss-unit">
+<div class="morpheme">mit</div>
+<div class="gloss">with</div>
+</div>
+<div class="gloss-unit">
+<div class="morpheme">Brot</div>
+<div class="gloss">bread</div>
+</div>
+</div>
+<div class="translation">‘Just a sandwich (as opposed to a fancier meal)’</div>
+</div>
+</div>
+</div>
