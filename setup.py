01b0269e2762375e5f3a1f9fe93b88b4b2b9052e
Johannes Englisch <englisch@shh.mpg.de>
2021-04-07 13:59:20 +0200

    bump version for development

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pydictionaria',
-    version='2.0',
+    version='2.0.dev0',
     description='',
     long_description='',
     classifiers=[
